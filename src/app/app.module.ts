e075b2d078a247e77cfbb42c9354dd594083ce4c
Saksham Koul <sxhmkoul@gmail.com>
2023-02-27 23:53:39 +0530

    base css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WorkIntervalComponent } from './components/work-interval/work-interval.component';
 import { BreakIntervalComponent } from './components/break-interval/break-interval.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { AnimateDirective } from './animate.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkIntervalComponent,
     BreakIntervalComponent,
-    TimerComponent
+    TimerComponent,
+    AnimateDirective
   ],
   imports: [
     BrowserModule,
